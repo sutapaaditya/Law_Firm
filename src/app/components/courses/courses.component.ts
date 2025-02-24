7d0349b9724eff88852682939540871c05e907e9
sutapaaditya <sutapaaditya84@gmail.com>
2025-02-24 23:59:32 +0530

    details page linked, another contact page done

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
